1e1f110b9e40cac6abb9ba340e275d11e57d4c02
Mulahasanovic <sead_m_bug@yahoo.com>
2015-03-31 22:41:12 +0200

    Update to Unity 5
    Old project deleted and replaced with the new one.

---
+++ Assets/Scripts/Controled_Push.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controled_Push : MonoBehaviour {
+
+	private Collider sphere;
+	private bool isInside = false;
+	//public GameObject bubbles;
+	//
+	//void OnStart(){
+	//	bubbles = GameObject.FindGameObjectWithTag("Bubbles");
+	//}
+	//
+	//void Update(){
+	//	if (Input.GetKey (KeyCode.Space)) {
+	//					if (bubbles != null)		
+	//			bubbles.particleSystem.Emit(10);
+	//			} else
+	//		if (bubbles != null)		
+	//			bubbles.particleSystem.Emit(0);
+	//}
+	void Update(){
+		if (isInside)
+			if(sphere != null)
+				if (Input.GetKey (KeyCode.Space))
+					sphere.GetComponent<Rigidbody>().AddForce (Vector3.up * Time.deltaTime * 1000);
+		
+	}
+	void OnTriggerEnter(Collider other){
+		sphere = other;
+		isInside = true;
+		
+	}
+	void OnTriggerExit(Collider other){
+		sphere = null;
+		isInside = false;
+	}
+}
---
+++ Assets/Scripts/Push.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Push : MonoBehaviour {
+
+	void OnTriggerStay(Collider other){
+		other.attachedRigidbody.AddForce(Vector3.up * Time.deltaTime * 1000);
+	}
+}
