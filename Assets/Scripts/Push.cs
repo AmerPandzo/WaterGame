c41435eded766bd294fb30fdeeaf191ce2e81f32
Mulahasanovic <sead.m.bug@gmail.com>
2017-03-15 22:47:26 +0100

    Restarting project from scrath with 5.5.2 version

--- Assets/Scripts/Push.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Push : MonoBehaviour {
-
-	void OnTriggerStay(Collider other){
-		other.attachedRigidbody.AddForce(Vector3.up * Time.deltaTime * 1000);
-	}
-}
