f7e76f3eddc42ec1a22fa057fa9aea3e17a53d7b
Mulahasanovic <sead.m.bug@gmail.com>
2017-03-15 23:36:49 +0100

    Primitive scene and canvas setup.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    [Header("UI Components")]
+    public Button playButton;
+    public Button quitButton;
+
+    void Awake()
+    {
+        playButton.onClick.AddListener(Play);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    private void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Quit();
+    }
+
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayManager : MonoBehaviour {
+
+    [Header("UI Components")]
+    public Button backButton;
+
+    void Awake()
+    {
+        backButton.onClick.AddListener(Back);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Back();
+    }
+
+    private void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
