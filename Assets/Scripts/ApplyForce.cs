9a1a02a29aa51125a3378c2f69d8a4efa37b5e54
Mulahasanovic <sead.m.bug@gmail.com>
2017-04-02 17:00:31 +0200

    Update for Unity 5.6 version and complex pressure force controls

--- Assets/Scripts/ApplyForce.cs
+++ Assets/Scripts/ApplyForce.cs
@@ -2,14 +2,34 @@
 
 public class ApplyForce : MonoBehaviour
 {
-    public float forceMagnitude = 1f;
+    public float maxForceMagnitude = 1f;
+    public float minForceMagnitude = 0.1f;
+    public float maxHoldDuration = 2f;
 
-    private bool isActive = false;
     private Vector3 up;
-
     void Awake()
     {
         up = gameObject.transform.up;
+        currentForceMagnitude = maxForceMagnitude;
+    }
+
+    public float currentForceMagnitude;
+    public float currentHoldDuration;
+    private bool isActive = false;
+    void Update()
+    {
+        if (isActive)
+        {
+            currentHoldDuration += Time.deltaTime;
+        }
+        else
+        {
+            currentHoldDuration -= Time.deltaTime;
+        }
+
+        currentHoldDuration = Mathf.Clamp(currentHoldDuration, 0, maxHoldDuration);
+
+        currentForceMagnitude = CalculateForce(currentHoldDuration);
     }
 
     void OnTriggerStay(Collider other)
@@ -18,9 +38,16 @@ public class ApplyForce : MonoBehaviour
         {
             if (other.attachedRigidbody)
             {
-                other.attachedRigidbody.AddForce(up * forceMagnitude);
+                other.attachedRigidbody.AddForce(up * currentForceMagnitude);
             }
         }
+    }
+
+    private float CalculateForce(float holdDuration)
+    {
+        float inverseForceToHoldDurtion = (holdDuration - maxHoldDuration) / (-maxHoldDuration);
+        float force = inverseForceToHoldDurtion <= 0 ? minForceMagnitude : inverseForceToHoldDurtion;
+        return force * maxForceMagnitude;
     }
 
     public void Toggle()
--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -8,6 +8,8 @@ public class PlayManager : MonoBehaviour
     public Button backButton;
     public Button leftPushButton;
     public Button rightPushButton;
+    public Slider leftPushSlider;
+    public Slider rightPushSlider;
 
     [Header("GameObject Pushers")]
     public GameObject leftPusher;
@@ -15,24 +17,41 @@ public class PlayManager : MonoBehaviour
 
     private ButtonHelper leftPushHelper;
     private ButtonHelper rightPushHelper;
+    private ApplyForce leftForce;
+    private ApplyForce rightForce;
 
     void Awake()
     {
         backButton.onClick.AddListener(Back);
 
+        leftPushSlider = leftPushButton.gameObject.GetComponentInChildren<Slider>();
+        rightPushSlider = rightPushButton.gameObject.GetComponentInChildren<Slider>();
+
+        leftForce = leftPusher.GetComponentInChildren<ApplyForce>();
+        rightForce = rightPusher.GetComponentInChildren<ApplyForce>();
+
+        leftPushSlider.maxValue = leftForce.maxForceMagnitude;
+        rightPushSlider.maxValue = rightForce.maxForceMagnitude;
+
         leftPushHelper = leftPushButton.GetComponent<ButtonHelper>();
         rightPushHelper = rightPushButton.GetComponent<ButtonHelper>();
 
-        leftPushHelper.OnPointerDownAction += leftPusher.GetComponentInChildren<ApplyForce>().Toggle;
-        leftPushHelper.OnPointerUpAction += leftPusher.GetComponentInChildren<ApplyForce>().Toggle;
-        rightPushHelper.OnPointerDownAction += rightPusher.GetComponentInChildren<ApplyForce>().Toggle;
-        rightPushHelper.OnPointerUpAction += rightPusher.GetComponentInChildren<ApplyForce>().Toggle;
+        leftPushHelper.OnPointerDownAction += leftForce.Toggle;
+        leftPushHelper.OnPointerUpAction += leftForce.Toggle;
+        rightPushHelper.OnPointerDownAction += rightForce.Toggle;
+        rightPushHelper.OnPointerUpAction += rightForce.Toggle;
 
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) Back();
+    }
+
+    void FixedUpdate()
+    {
+        leftPushSlider.value = leftForce.currentForceMagnitude;
+        rightPushSlider.value = rightForce.currentForceMagnitude;
     }
 
     private void Back()
